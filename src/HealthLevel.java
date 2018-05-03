cbe9edd43b12a96b3464970c110121ac86ce32ba
Nona_Rjb <rajabi_nona@ee.sharif.edu>
2018-05-03 11:37:33 +0430

    first commit\nCoordinate.java\nHealthLevel.java\nInvader.java

---
+++ src/HealthLevel.java
@@ -0,0 +1,20 @@
+public class HealthLevel {
+    private int healthLevel;
+
+    public int getHealthLevel() {
+        return healthLevel;
+    }
+
+    public void setHealthLevel(int healthLevel) {
+        this.healthLevel = healthLevel;
+    }
+
+    public void decreaseHealth(int percentage){
+        this.healthLevel -= percentage;
+    }
+
+    public void increaseHealth(int percentage){
+        this.healthLevel += percentage;
+    }
+}
+
---
+++ src/Invader.java
@@ -0,0 +1,12 @@
+abstract class Invader {
+    Coordinate coordinate;
+    HealthLevel healthLevel;
+    int level;
+    int delayConst;
+    int Speed;
+    int range;
+    int xpCnt;
+    Invader targegtInvader;
+    int graphicalSize;
+    int numberOfKillings;
+}
