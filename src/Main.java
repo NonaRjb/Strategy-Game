a6621d580cd513b1364efbfd8c2a735898013a1f
AliFathi <fathi_ali@ee.sharif.edu>
2018-05-03 13:50:14 +0430

    Very Good

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello Nona");
+	    System.out.println("Hello!");
         System.out.println("... :))");
     }
 }
