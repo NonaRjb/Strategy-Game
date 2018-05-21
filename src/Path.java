fb153acac6bd330ea60cb4843db6db237aed8645
Nona_Rjb <rajabi_nona@ee.sharif.edu>
2018-05-21 20:27:08 +0430

    10th commit: Path.java is added, nextCoordinate method

---
+++ src/Path.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Path {
+
+    private ArrayList<Coordinate> pathWay = new ArrayList<>();
+
+    // Constructor
+    public Path() {}
+
+    // Other Methods
+    public void buildPath(int x, int y){
+        pathWay.add( new Coordinate(x,y) );
+    }
+
+    // checks if the path contains the given coordinate
+    public Boolean isInWay(Coordinate coordinate){
+        return pathWay.contains(coordinate);
+    }
+
+    // gives the next coordinate of a given coordinate of a path
+    // if the given coordinate is the last coordinate of the path the method returns (450,1599)
+    public Coordinate nextCoordinate(Coordinate coordinate){
+        int index = pathWay.indexOf(coordinate);
+        if(pathWay.size()-1 == index){
+            Coordinate nextCoordinate = new Coordinate(450, 1599);
+            return nextCoordinate;
+        }
+        else {
+            return pathWay.get(index+1);
+        }
+    }
+}
---
+++ src/PlayGround.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class PlayGround {
+    final int numberOfPlaces = 3;
+    final int numberOfOrigins = 2;
+    Map map = new Map();
+
+    Coordinate[] placeOfArmories = {new Coordinate(1,1),
+                                    new Coordinate(2,2),
+                                    new Coordinate(3,3)};
+
+    Coordinate[] pathOriginCoordinate = {new Coordinate(1,2),
+                                        new Coordinate(3,4)};
+
+    PlaceHolder[] placeHolder = new PlaceHolder[numberOfPlaces];
+
+    // Constructor
+    public PlayGround() {
+        for( int i=0; i<numberOfPlaces; i++ ){
+            this.placeHolder[i] = new PlaceHolder( i,placeOfArmories[i] );
+        }
+    }
+
+    // Getters
+    public PlaceHolder getPlaceHolder(int id) {
+        return placeHolder[id];
+    }
+
+    // Other Methods
+    public Coordinate randomOriginMaker(){
+        Random rand = new Random();
+        int placeToEnter = rand.nextInt( numberOfOrigins );
+        return this.pathOriginCoordinate[ placeToEnter ];
+    }
+
+    // returns the next coordinate of a given coordinate
+    // if the given coordinate is the last coordinate of the path the method returns (450,1599)
+    public Coordinate nextCoordinate (Coordinate currentCoordinate){
+        return map.nextCoordinate(currentCoordinate);
+    }
+
+}
